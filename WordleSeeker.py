676077635923264ffa46eef75a1cbdc1c5925d7c
Voltaic314 <104940692+Voltaic314@users.noreply.github.com>
2022-05-06 00:23:03 -0400

    Update WordleSeeker.py

--- WordleSeeker.py
+++ WordleSeeker.py
@@ -1,45 +1,36 @@
+## This code was created by Logan Maupin - Student at the university of Arizona Online. Studying computer science for AI & data science. 
+## If you would like to reach out to me directly, please just use my student email which is -- lmaupin@email.arizona.edu -- Thank you. Enjoy! :) 
+
+
 # open and read the dictionary txt file
 f = open('words_alpha_5only.txt', 'r')
 with open('words_alpha_5only.txt') as f:
     lines = f.readlines() # lines variable reads all lines in dictionary file
     stripped_lines = [s.strip() for s in lines] # stripped lines represents all words minus the \n line break
+# if you print(stripped_lines) you will print all 5 letter words without any filteration... and yes if you are wondering the master list contains 15,920 words. 
 
-five_letter_words = [] # creates an empty list to fill
-for i in stripped_lines: # for loop
-        five_letter_words.append(i) # fill the empty list with words from the text file
 
+#create loop to hold everything. Break loop if all bottom conditions are a success. 
 running = True
 while running:
 
     filtered = [] #creates another empty list for the user input letters
-
     #input up to 5 lines of string combos. Can not exceed more than 5 total characters (duh). use one line for each letter or set of letters (set meaning letters next to each other).
-    substring1 = input("Please enter the first letter you know: ").lower()
-    substring2 = input("Please enter the second letter you know - (If you don't have a second, just hit enter): ").lower()
-    substring3 = input("Please enter the third letter you know - (If you don't have a third, just hit enter): ").lower()
-    substring4 = input("Please enter the fourth letter you know - (If you don't have a fourth, just hit enter): ").lower()
-    substring5 = input("Please enter the fifth letter you know - (If you don't have a fifth, just hit enter): ").lower()
-
-        print("\nWARNING: DO NOT HIT ENTER WITH NO INPUT ON THE NEXT INPUTS\n WARNING: DO NOT HIT ENTER WITH NO INPUT ON THE NEXT INPUTS\n WARNING: DO NOT HIT ENTER WITH NO INPUT ON THE NEXT INPUTS\n WARNING: DO NOT HIT ENTER WITH NO INPUT ON THE NEXT INPUTS\n WARNING: DO NOT HIT ENTER WITH NO INPUT ON THE NEXT INPUTS\n ")
-
-    # Taking multiple inputs
-    bad_1 = input("Please input a letter you know is not in the word - If you don't have one, type space and hit enter: ").lower()
-    bad_2 = input("Please input a letter you know is not in the word - If you don't have one, type space and hit enter: ").lower()
-    bad_3 = input("Please input a letter you know is not in the word - If you don't have one, type space and hit enter: ").lower()
-    bad_4 = input("Please input a letter you know is not in the word - If you don't have one, type space and hit enter: ").lower()
-    bad_5 = input("Please input a letter you know is not in the word - If you don't have one, type space and hit enter: ").lower()
-    bad_6 = input("Please input a letter you know is not in the word - If you don't have one, type space and hit enter: ").lower()
-    bad_7 = input("Please input a letter you know is not in the word - If you don't have one, type space and hit enter: ").lower()
-    bad_8 = input("Please input a letter you know is not in the word - If you don't have one, type space and hit enter: ").lower()
+    good_list = [str(x).lower() for x in input("Enter any letters you know that are in the word - (separate letters with a comma and space): \n").split(', ')]
     
-    # adds up the length of characters from each input of the 5 user input lines
-    length_of_all = len(substring1) + len(substring2) + len(substring3) + len(substring4) + len(substring5)
+    #the purpose of this for loop is to add up all the string elements letter by letter
+    for element in good_list:
+        len(element)
+    length_of_all_good = len(element) #defining the variable that will be used below, wanted to make sure this variable gets defined outside of the loop just in case. 
 
-#if length of all 5 user inputs is more than 5...
-    if length_of_all <= 5:
-            for string in five_letter_words:
-        # this next line is basically saying look for all strings in the latter 5 (or less) user inputs, and search them against the 5 letter word list
-                if substring1 in string and substring2 in string and substring3 in string and substring4 in string and substring5 in string and bad_1 not in string and bad_2 not in string and bad_3 not in string and bad_4 not in string and bad_5 not in string and bad_6 not in string and bad_7 not in string and bad_8 not in string:
+    #Input all the bad letters and put them in their own list for later use. 
+    bad_list = [str(x).lower() for x in input("Enter any letters that you know are NOT in the list - (separate letters with a comma and space, also if you don't know any bad letters just hit space and then enter):  \n").split(', ')]
+
+#if length_of_all_good inputs is more than 5 then tell the user they are a moron, if not proceed with stripping word list.
+    if length_of_all_good <= 5:
+            for string in stripped_lines:
+            # this next line is basically saying look for all strings in the latter 5 (or less) user inputs, and search them against the 5 letter word list
+                if all(x in string for x in good_list) and not any(x in string for x in bad_list): #compare good list to master list and compare bad list to master list. 
                     filtered.append(string)  # modify the word list to *only* include words that contain the matching letters from the (up to) 5 inputs.
                     running = False #break out of the loop -- success!
     else: #tell the user they are a moron and to try again.
@@ -47,3 +38,5 @@ while running:
 
 # print the output of all words matching the str texts from the previous if (success) function
 print('\n'.join(map(str, filtered)))  # print final output of 5 letter words matching the letters provided in the user input
+number_of_items = len(filtered) # number of words in the search result
+print("This search has resulted in " + str(number_of_items) + " match(es) to your query.") # prints number string plus var plus string
