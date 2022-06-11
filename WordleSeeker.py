77d924b6530a508736fc38328b70bdc61b4d9586
Voltaic314 <104940692+Voltaic314@users.noreply.github.com>
2022-06-11 19:42:56 -0400

    Cleaned up the code a bit more

--- WordleSeeker.py
+++ WordleSeeker.py
@@ -9,8 +9,6 @@ with open('words_alpha_5only.txt') as f:
     stripped_lines = [s.strip() for s in lines] # stripped lines represents all words minus the \n line break
 # if you print(stripped_lines) you will print all 5 letter words without any filteration... and yes if you are wondering the master list contains 15,920 words. 
 
-f.close()
-
 #create loop to hold everything. Break loop if all bottom conditions are a success. 
 running = True
 while running:
@@ -20,22 +18,23 @@ while running:
     good_list = [str(x).lower() for x in input("Enter any letters you know that are in the word - (separate letters with a comma and space): \n").split(', ')]
     
     #the purpose of this for loop is to add up all the string elements letter by letter
-    for element in good_list:
-        len(element)
-    length_of_all_good = len(element) #defining the variable that will be used below, wanted to make sure this variable gets defined outside of the loop just in case. 
+    for letters in good_list:
+        len(letters)
+    length_of_all_good = len(elements) #defining the variable that will be used below, wanted to make sure this variable gets defined outside of the loop just in case. 
 
     #Input all the bad letters and put them in their own list for later use. 
-    bad_list = [str(x).lower() for x in input("Enter any letters that you know are NOT in the list - (separate letters with a comma and space, also if you don't know any bad letters just hit space and then enter):  \n").split(', ')]
+    bad_list = [str(letters).lower() for letters in input("Enter any letters that you know are NOT in the list - (separate letters with a comma and space, also if you don't know any bad letters just hit space and then enter):  \n").split(', ')]
 
 #if length_of_all_good inputs is more than 5 then tell the user they are a moron, if not proceed with stripping word list.
     if length_of_all_good <= 5:
             for string in stripped_lines:
             # this next line is basically saying look for all strings in the latter 5 (or less) user inputs, and search them against the 5 letter word list
-                if all(x in string for x in good_list) and not any(x in string for x in bad_list): #compare good list to master list and compare bad list to master list. 
+                if all(letters in string for letters in good_list) and not any(letters in string for letters in bad_list): #compare good list to master list and compare bad list to master list. 
                     filtered.append(string)  # modify the word list to *only* include words that contain the matching letters from the (up to) 5 inputs.
                     running = False #break out of the loop -- success!
     else: #tell the user they are a moron and to try again.
         print("Too many characters! Total of all 5 lines must be less than or equal to 5! Please try again:")
+        continue
 
 # print the output of all words matching the str texts from the previous if (success) function
 print('\n'.join(map(str, filtered)))  # print final output of 5 letter words matching the letters provided in the user input
