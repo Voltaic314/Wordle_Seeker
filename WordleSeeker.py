7f95dab1b7b63c544d4357f26af4065542fdcb71
Logan Maupin <104940692+Voltaic314@users.noreply.github.com>
2022-10-11 10:35:34 -0400

    currently breaking stuff :)

--- WordleSeeker.py
+++ WordleSeeker.py
@@ -1,5 +1,7 @@
 ## This code was created by Logan Maupin - Student at the university of Arizona Online. Studying computer science for AI & data science.
 
+from typing import Iterable
+
 def read_txt_file(word_list):
 # open and read the dictionary txt file
     open(word_list, 'r')
@@ -7,26 +9,39 @@ def read_txt_file(word_list):
         lines = f.readlines()
         return [s.strip() for s in lines]
 
+def find_word_for(collection: list[str], char: str, index: int) -> Iterable[str]:
+    for word in collection:
+        if len(word)>=index-1 and word[index]==char:
+            yield word
 
 def filter_by_letter_position(original_list):
+    new_list = []
     running = True
     while running:
-        letter_position_question = str(input("Do you know where any of the good letters' positions are? Y/N")).upper()
+        letter_position_question = str(input("Do you know where any of the good letters' positions are? Y/N: ")).upper()
         if letter_position_question == "Y":
             what_letter = str(input("What is the first letter you know?")).lower()
-            where_is_that_letter = int(input("Where is that letter located in the word? (1-5)")) + 1
-            new_list = []
-            running = False
+            where_is_that_letter = int(input("Where is that letter located in the word? (1-5)"))
+            for word in original_list:
+                if word[where_is_that_letter-1] == what_letter:
+                    new_list.append(word)
+            filter_by_another_letter_question = str(input("Do you know the position of another letter? Y/N: ")).upper()
+            if filter_by_another_letter_question == "Y":
+                original_list = new_list
+                continue
 
+            elif filter_by_another_letter_question == "N":
+                break
+
+            else:
+                print("I didn't quite catch that, do you know the position of another letter? Y or N: ")
 
         elif letter_position_question == "N":
             break
+
         else:
             print("I didn't quite catch that. Do you know where any letter positions are?")
             continue
-
-        print(len(new_list))
-        print(new_list)
 
 
 def filter_list_by_letters():
@@ -70,6 +85,8 @@ def return_filtered_list_by_letters(original_list, good_letters, bad_letters):
 def main():
     original_list = read_txt_file('words_alpha_5only.txt')
     filter_by_letter_position(original_list)
+    print(len(new_list))
+    print(new_list)
 
 
 if __name__ == "__main__":
