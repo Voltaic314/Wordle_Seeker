59d1999c201e456de25d271373382dbc322a2094
Voltaic314 <104940692+Voltaic314@users.noreply.github.com>
2022-10-23 12:02:54 -0400

    breaking stuff

--- WordleSeeker.py
+++ WordleSeeker.py
@@ -14,27 +14,29 @@ def find_word_for(collection: list[str], char: str, index: int) -> Iterable[str]
         if len(word)>=index-1 and word[index]==char:
             yield word
 
-def filter_by_letter_position(original_list):
-    new_list = []
+def filter_by_letter_position(original_list: list[str]):
+    list_of_words_we_want = []
     running = True
     while running:
         letter_position_question = str(input("Do you know where any of the good letters' positions are? Y/N: ")).upper()
         if letter_position_question == "Y":
-            what_letter = str(input("What is the first letter you know?")).lower()
-            where_is_that_letter = int(input("Where is that letter located in the word? (1-5)"))
+            what_letter = str(input("What letter do you know the position of?")).lower()
+            where_is_that_letter = int(input("Where is that letter located in the word? (1-5)")) - 1
             for word in original_list:
-                if word[where_is_that_letter-1] == what_letter:
-                    new_list.append(word)
-            filter_by_another_letter_question = str(input("Do you know the position of another letter? Y/N: ")).upper()
+                if word[where_is_that_letter] == what_letter:
+                    list_of_words_we_want.append(word)
+
+            filter_by_another_letter_question = str(input("Do you want to add another letter's position? Y/N: ")).upper()
             if filter_by_another_letter_question == "Y":
-                original_list = new_list
                 continue
 
             elif filter_by_another_letter_question == "N":
+                return original_list
                 break
 
             else:
                 print("I didn't quite catch that, do you know the position of another letter? Y or N: ")
+                continue
 
         elif letter_position_question == "N":
             break
@@ -46,11 +48,11 @@ def filter_by_letter_position(original_list):
 
 def filter_list_by_letters():
     good_list = [str(x).lower() for x in input(
-        'Enter any letters you know that are in the word - (separate letters with a comma and space): \n').split(', ')]
+        'Enter any other letters you know that are in the word - (separate letters with a comma and space): \n').split(', ')]
     length_of_all_good = len(good_list)
 
     bad_list = [str(letters).lower() for letters in input(
-        'Enter any letters that you know are NOT in the list - (separate letters with a comma and space, also if you don\'t know any bad letters just hit space and then enter):  \n').split(', ')]
+        'Enter any letters that you know are NOT in the word - (separate letters with a comma and space, also if you don\'t know any bad letters just hit space and then enter):  \n').split(', ')]
     return length_of_all_good, good_list, bad_list
 
 
@@ -63,8 +65,8 @@ def return_filtered_list_by_letters(original_list, good_letters, bad_letters):
         if len(good_letters) <= 5:
             for string in original_list:
 
-                if all(letters in string for letters in good_letters) and not any(letters in string for letters in bad_letters):  # compare good list to master list and compare bad list to master list.
-                    filtered.append(string)
+                if not all(letters in string for letters in good_letters) and any(letters in string for letters in bad_letters):  # compare good list to master list and compare bad list to master list.
+                    original_list.remove(string)
 
                 running = False
 
@@ -73,8 +75,8 @@ def return_filtered_list_by_letters(original_list, good_letters, bad_letters):
             continue
 
     # print the output of all words matching the str texts from the previous if (success) function
-    print('\n'.join(map(str, filtered)))
-    number_of_items = len(filtered)  # number of words in the search result
+    print('\n'.join(map(str, original_list)))
+    number_of_items = len(original_list)  # number of words in the search result
     if number_of_items != 1:
         print("This search has resulted in " + str(number_of_items) + " matches to your query.")  # prints number string plus var plus string
     else:
@@ -85,8 +87,8 @@ def return_filtered_list_by_letters(original_list, good_letters, bad_letters):
 def main():
     original_list = read_txt_file('words_alpha_5only.txt')
     filter_by_letter_position(original_list)
-    print(len(new_list))
-    print(new_list)
+    good_list_length, good_list, bad_list = filter_list_by_letters()
+    return_filtered_list_by_letters(original_list, good_list, bad_list)
 
 
 if __name__ == "__main__":
